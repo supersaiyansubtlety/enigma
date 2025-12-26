a0393723dacad24aaefc443189452ddaa4897b80
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-12-26 15:06:31 -0800

    remove Pair's equals and hashCode overrides; they were inferior to records' defaults

--- enigma/src/main/java/org/quiltmc/enigma/util/Pair.java
+++ enigma/src/main/java/org/quiltmc/enigma/util/Pair.java
@@ -1,17 +1,3 @@
 package org.quiltmc.enigma.util;
 
-import java.util.Objects;
-
-public record Pair<A, B>(A a, B b) {
-	@Override
-	public int hashCode() {
-		return this.a.hashCode() ^ this.b.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		return o instanceof Pair
-				&& Objects.equals(this.a, ((Pair<?, ?>) o).a)
-				&& Objects.equals(this.b, ((Pair<?, ?>) o).b);
-	}
-}
+public record Pair<A, B>(A a, B b) { }
