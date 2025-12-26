eaef024e4c8f616152eaf23e9f4ac8081f4cc563
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-12-26 14:23:45 -0800

    update names in SearchUtilTest: wordwise -> wordEnd

--- enigma-swing/src/test/java/org/quiltmc/enigma/gui/search/SearchUtilTest.java
+++ enigma-swing/src/test/java/org/quiltmc/enigma/gui/search/SearchUtilTest.java
@@ -10,38 +10,38 @@ import static org.hamcrest.Matchers.arrayContaining;
 
 public class SearchUtilTest {
 	@ParameterizedTest
-	@MethodSource("streamWordwiseExpectations")
-	void testWordwiseSplit(WordwiseExpectation expectation) {
-		assertThat(SearchUtil.Entry.WORD_END.split(expectation.input), arrayContaining(expectation.output));
+	@MethodSource("streamWordEndExpectations")
+	void testWordEnd(WordEndExpectation expectation) {
+		assertThat(SearchUtil.Entry.WORD_END.split(expectation.input), arrayContaining(expectation.split));
 	}
 
-	private static Stream<WordwiseExpectation> streamWordwiseExpectations() {
+	private static Stream<WordEndExpectation> streamWordEndExpectations() {
 		return Stream.of(
-			WordwiseExpectation.of("single", "single"),
-			WordwiseExpectation.of("SINGLE", "SINGLE"),
-			WordwiseExpectation.of("UpperCamelCase", "Upper", "Camel", "Case"),
-			WordwiseExpectation.of("lowerCamelCase", "lower", "Camel", "Case"),
-			WordwiseExpectation.of("SCREAMUpperCamel", "SCREAM", "Upper", "Camel"),
-			WordwiseExpectation.of("lowerSCREAMCamel", "lower", "SCREAM", "Camel"),
-			WordwiseExpectation.of("lowerCamelSCREAM", "lower", "Camel", "SCREAM"),
-			WordwiseExpectation.of("lower_snake_case", "lower", "_", "snake", "_", "case"),
-			WordwiseExpectation.of("SCREAMING_SNAKE_CASE", "SCREAMING", "_", "SNAKE", "_", "CASE"),
-			WordwiseExpectation.of("class_932", "class", "_", "932"),
-			WordwiseExpectation.of("X11FontManager", "X", "11", "Font", "Manager")
+			WordEndExpectation.of("single", "single"),
+			WordEndExpectation.of("SINGLE", "SINGLE"),
+			WordEndExpectation.of("UpperCamelCase", "Upper", "Camel", "Case"),
+			WordEndExpectation.of("lowerCamelCase", "lower", "Camel", "Case"),
+			WordEndExpectation.of("SCREAMUpperCamel", "SCREAM", "Upper", "Camel"),
+			WordEndExpectation.of("lowerSCREAMCamel", "lower", "SCREAM", "Camel"),
+			WordEndExpectation.of("lowerCamelSCREAM", "lower", "Camel", "SCREAM"),
+			WordEndExpectation.of("lower_snake_case", "lower", "_", "snake", "_", "case"),
+			WordEndExpectation.of("SCREAMING_SNAKE_CASE", "SCREAMING", "_", "SNAKE", "_", "CASE"),
+			WordEndExpectation.of("class_932", "class", "_", "932"),
+			WordEndExpectation.of("X11FontManager", "X", "11", "Font", "Manager")
 		);
 	}
 
-	record WordwiseExpectation(String input, String... output) {
-		private static WordwiseExpectation of(String input, String... output) {
-			if (output.length < 1) {
-				throw new IllegalArgumentException("output must not be empty!");
+	record WordEndExpectation(String input, String... split) {
+		private static WordEndExpectation of(String input, String... split) {
+			if (split.length < 1) {
+				throw new IllegalArgumentException("split must not be empty!");
 			}
 
-			if (!String.join("", output).equals(input)) {
-				throw new IllegalArgumentException("output must be concatenation of input!");
+			if (!String.join("", split).equals(input)) {
+				throw new IllegalArgumentException("split must be concatenation of input!");
 			}
 
-			return new WordwiseExpectation(input, output);
+			return new WordEndExpectation(input, split);
 		}
 	}
 }
